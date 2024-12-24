2e8cbb80d197fa28428fd7ee83a5fcbb0441789e
joselrg_ <jr7035960@gmail.com>
2024-12-23 23:39:26 -0600

    Avances vista producto y factura

---
+++ InvenSyncWeb/Controllers/FacturaVentaController.cs
@@ -0,0 +1,43 @@
+﻿using CapaDatosWeb.Modelado;
+using CapaNegocioWeb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvenSyncWeb.Controllers
+{
+    public class FacturaVentaController : BaseController
+    {
+        private readonly InvenSyncEntity _db;
+        private readonly FacturaVentaService _facturaVentaService;
+
+        public FacturaVentaController()
+        {
+            _db = new InvenSyncEntity();
+            _facturaVentaService = new FacturaVentaService(_db);
+        }
+
+        // GET: FacturaVenta
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarFacturaVenta()
+        {
+            try
+            {
+                var facturVenta = _facturaVentaService.GetAll();
+                return CreateResponse(true, "Factura Venta generada exitosamente", facturVenta);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex, "Error al listar Factura Venta.");
+            }
+        }
+    }
+}
---
+++ InvenSyncWeb/Controllers/ProductoController.cs
@@ -0,0 +1,43 @@
+﻿using CapaDatosWeb.Modelado;
+using CapaNegocioWeb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvenSyncWeb.Controllers
+{
+    public class ProductoController : BaseController
+    {
+
+        private readonly InvenSyncEntity _db;
+        private readonly ProductoServices _productoService;
+
+        public ProductoController()
+        {
+            _db = new InvenSyncEntity();
+            _productoService = new ProductoServices(_db);
+        }
+
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarProducto()
+        {
+            try
+            {
+                var producto = _productoService.GetAll();
+                return CreateResponse(true, "Producto generado exitosamente", producto);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex, "Error al listar producto.");
+            }
+        }
+    }
+}
